3c68dbeabbc0610abd9f3b61237bfaed5a29b13f
Rolando <rolandou548@gmail.com>
2023-08-22 22:27:23 -0400

    First commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,44 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
-// include your styles into the webpack bundle
 import "../styles/index.css";
+import { func } from "prop-types";
 
-//import your own components
-import Home from "./component/home.jsx";
+function SimpleCounter(props) {
+  return (
+    <div className="bigCounter">
+      <div className="calendar">
+        <i className="fa-regular fa-clock"></i>
+      </div>
+      <div className="four">{props.four % 10}</div>
+      <div className="three">{props.three % 10}</div>
+      <div className="two">{props.two % 10}</div>
+      <div className="one">{props.one % 10}</div>
+    </div>
+  );
+}
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+SimpleCounter.propTypes = {
+  one: PropTypes.number,
+  two: PropTypes.number,
+  three: PropTypes.number,
+  four: PropTypes.number,
+};
+
+let counter = 0;
+
+setInterval(function () {
+  const one = Math.floor(counter / 10);
+  const two = Math.floor(counter / 100);
+  const three = Math.floor(counter / 1000);
+  const four = Math.floor(counter / 10000);
+  console.log(one, two, three, four);
+  counter++;
+
+  ReactDOM.render(
+    <SimpleCounter one={one} two={two} three={three} four={four} />,
+    document.getElementById("app")
+  );
+}, 100);
